9c85e4c9112063df373b8294e0d76a4c9e956b00
shubham <shubh.shubham21@gmail.com>
2022-05-17 12:15:21 +0530

    initail project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/hotels' element={<List />}></Route>
+				<Route path='/hotels/:id' element={<Hotel />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
