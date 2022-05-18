21116b4dc2e4cc62844a507c40c8c31151d5c269
shubham <shubh.shubham21@gmail.com>
2022-05-18 12:11:42 +0530

    UI has completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './navbar.css';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className='navbar'>
+			<div className='navContainer'>
+				<span className='logo'>Hotelbooking</span>
+				<div className='navItems'>
+					<button className='navButton'>Register</button>
+					<button className='navButton'>Login</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
